005a1698dd79180414f63b48641b1fc319354f4d
Xin Wang <kevin.w.xin@gmail.com>
2020-05-18 17:28:32 +0000

    fix create_image.sh version issue

--- create_images.sh
+++ create_images.sh
@@ -9,6 +9,6 @@ docker push 192.168.1.1/disk-partition:v1
 docker build -t 192.168.1.1/install-root-fs:v101 03-install-root-fs/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/install-root-fs:v1
 docker build -t 192.168.1.1/install-grub:v1 04-install-grub/ --build-arg REGISTRY=192.168.1.1
-docker push 192.168.1.1/install-grub:v101
+docker push 192.168.1.1/install-grub:v1
 docker build -t 192.168.1.1/cloud-init:v1 05-cloud-init/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/cloud-init:v1
