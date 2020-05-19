c1d9ab1b38ea5a357e3ea175859c645bbc53b0dc
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-05-19 10:33:49 +0100

    Fix steps for creating image and other typos
    * Credit the original authors (Packet.net)
    https://github.com/tinkerbell/tink/tree/first-good-workflow/workflow-samples/ubuntu
    * Credit additional author for fixes / changes
    * Document how to build an image
    * Fix create_images.sh which were pushing the wrong image
    tag
    * Use bash and interpolation instead of hard-coded examples
    * Add license for material
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- create_images.sh
+++ create_images.sh
@@ -2,13 +2,18 @@
   
 docker build -t 192.168.1.1/ubuntu:base 00-base/
 docker push 192.168.1.1/ubuntu:base
+
 docker build -t 192.168.1.1/disk-wipe:v1 01-disk-wipe/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/disk-wipe:v1
-docker build -t 192.168.1.1/disk-partition:v101 02-disk-partition/ --build-arg REGISTRY=192.168.1.1
+
+docker build -t 192.168.1.1/disk-partition:v1 02-disk-partition/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/disk-partition:v1
-docker build -t 192.168.1.1/install-root-fs:v101 03-install-root-fs/ --build-arg REGISTRY=192.168.1.1
+
+docker build -t 192.168.1.1/install-root-fs:v1 03-install-root-fs/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/install-root-fs:v1
+
 docker build -t 192.168.1.1/install-grub:v1 04-install-grub/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/install-grub:v1
+
 docker build -t 192.168.1.1/cloud-init:v1 05-cloud-init/ --build-arg REGISTRY=192.168.1.1
 docker push 192.168.1.1/cloud-init:v1
